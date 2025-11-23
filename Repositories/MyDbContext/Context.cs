e17fb5f92fab03556d006b875b86ee5f8df6def8
lauchiwai <lauuai5244@yahoo.com.hk>
2025-11-23 16:15:39 +0800

    Initial commit for Rbac_Review project

---
+++ Repositories/MyDbContext/Context.cs
@@ -0,0 +1,25 @@
+﻿using Common.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositories.MyDbContext;
+
+public partial class Context : DbContext
+{
+    public Context(DbContextOptions<Context> options) : base(options)
+    { }
+
+    public DbSet<Roles> Roles { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Roles>(entity =>
+        {
+            entity.HasKey(e => e.RoleId);
+            entity.Property(e => e.RoleId)
+                  .ValueGeneratedOnAdd();
+            entity.Property(e => e.RoleName)
+                  .IsRequired()
+                  .HasMaxLength(255);
+        });
+    }
+}
---
+++ rbac_reviews/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Repositories.MyDbContext;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services.AddDbContext<Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")),
+    ServiceLifetime.Scoped);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
